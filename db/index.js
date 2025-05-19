52c72a52ab2b0491a787d9e453d14e42d6c72915
arnaldiarif <arnaldiarif2812@gmail.com>
2025-05-19 13:11:41 +0700

    Initial commit - ATM CLI simulation project

--- db/index.js
+++ db/index.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',         // Ganti jika berbeda
-  password: '281202',         // Ganti jika ada password
-  database: 'atm'       // Nama DB sesuai perintah
+  user: 'root',       
+  password: '281202',        
+  database: 'atm'      
 });
 
 module.exports = pool;
